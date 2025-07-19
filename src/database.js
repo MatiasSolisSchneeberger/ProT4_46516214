00d2fc29215ea30465f016509937de393065f71a
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-07-18 21:50:32 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const properties = {
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'rest-api'
+    database: 'biblioteca'
 };
 
 export const pool = mysqlConnection.createPool(properties);
